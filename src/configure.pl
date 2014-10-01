6cbef1251ef59796af0c473c4c050ee148930d59
g1257 <gz1@ornl.gov>
2014-10-01 15:23:09 -0400

    configure.pl uses Make.pm

--- src/configure.pl
+++ src/configure.pl
@@ -1,7 +1,6 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 =pod
-// BEGIN LICENSE BLOCK
-Copyright (c) 2009-2012, UT-Battelle, LLC
+Copyright (c) 2009-2014, UT-Battelle, LLC
 All rights reserved
 
 [Lanczos++, Version 1.0.0]
@@ -11,117 +10,37 @@ THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
 CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-PARTICULAR PURPOSE ARE DISCLAIMED. 
+PARTICULAR PURPOSE ARE DISCLAIMED.
 
 Please see full open source license included in file LICENSE.
 ---------------------------------------------------------------
-// END LICENSE BLOCK
 
 =cut
 use warnings;
 use strict;
 
-my $hasGsl = "no"; # say "no" here to remove GSL dependence
+use lib "../../PsimagLite/scripts";
+use Make;
 
 my $mpi=0;
-my $platform="linux";
-my $lapack="-llapack";
+my $platform=guessPlatform();
+my @drivers = ("lanczos");
+my $lapack=Make::findLapack();
 my $PsimagLite="../../PsimagLite";
 my ($pthreads,$pthreadsLib)=(0,"");
 my $brand= "v1.0";
+my $build="production";
+my $floating="";
 
-
-my $gslLibs = " -lgsl  -lgslcblas ";
-$gslLibs =" " if ($hasGsl=~/n/i);
-
-#guessPlatform();
-
-#welcome();
-
-#my $model="";
-#my $modelLocation="";
-#my $stored="";
-#askQuestions();
+system("make clean");
 
 createMakefile();
 
-#createDriver();
-
-
-sub createMakefile
-{
-	system("cp Makefile Makefile.bak") if (-r "Makefile");
-	my $compiler = compilerName();
-	open(FOUT,">Makefile") or die "Cannot open Makefile for writing: $!\n";
-	my $usePthreadsOrNot = " ";
-	$usePthreadsOrNot = " -DUSE_PTHREADS " if ($pthreads);
-
-print FOUT<<EOF;
-# DO NOT EDIT!!! Changes will be lost. Modify configure.pl instead
-# This Makefile was written by configure.pl
-# Lanczos++ ($brand) by G.A.
-# Platform: $platform
-# MPI: $mpi
-
-LDFLAGS =    $lapack  $gslLibs $pthreadsLib
-CPPFLAGS = -Werror -Wall -Wstrict-overflow=5 -IEngine \\
-           -IModels/Tj1Orb -IModels/Immm -IModels/HubbardOneOrbital \\
-           -IModels/FeBasedSc -I$PsimagLite -I$PsimagLite/src $usePthreadsOrNot
-EOF
-if ($mpi) {
-	print FOUT "CXX = mpicxx -O3 -DNDEBUG \n";
-} else {
-	print FOUT "CXX = $compiler  -O3 -DNDEBUG\n";
-	print FOUT "#Comment out line below for debugging: \n";
-	print FOUT "#CXX = $compiler -g3 \n";
-	print FOUT "#Comment out line below for valgrind callgrind analysis: \n";
-	print FOUT "#CXX = $compiler -g3 -DNDEBUG -O1 \n";
-}
-print FOUT<<EOF;
-EXENAME = lanczos
-all: \$(EXENAME)
-
-lanczos.cpp: configure.pl
-	perl configure.pl
-
-lanczos:  lanczos.o 
-	\$(CXX) -o lanczos lanczos.o \$(LDFLAGS)  
-
-# dependencies brought about by Makefile.dep
-%.o: %.cpp Makefile
-	\$(CXX) \$(CPPFLAGS) -c \$< 
-
-Makefile.dep: lanczos.cpp
-	\$(CXX) \$(CPPFLAGS) -MM lanczos.cpp  > Makefile.dep
-
-clean:
-	rm -f core* \$(EXENAME) *.o Makefile.dep
-
-include Makefile.dep
-
-######## End of Makefile ########
-
-EOF
-	close(FOUT);
-	print STDERR "File Makefile has been written\n";
-}
-
-
-sub computeBackwardMovements
-{
-	my ($directory) = @_;
-	my $ret = "";
-	while ($directory =~ s/\///) {
-		$ret = $ret."../";
-	}
-	return $ret;	
-}
-
-
 sub guessPlatform
 {
-	$platform="Linux";
+	my $platform="Linux";
 	$platform="Darwin" if (isAMac());
+	return $platform;
 }
 
 sub isAMac
@@ -129,50 +48,44 @@ sub isAMac
 	open(PIPE,"uname -a |grep -i Darwin") or return 0;
 	$_=<PIPE>;
 	close(PIPE);
-	
+
 	return 1 unless ($_ eq "" or $_ eq "\n");
 	return 0;
 }
 
-
-sub getPthreadsName
+sub createMakefile
 {
-	my $pthreadsName = "UNKNOWN";
-	if ($pthreads) {
-		$pthreadsName = "Pthreads";
-	} else {
-		$pthreadsName = "NoPthreads";
-	}
-	return $pthreadsName;
+	unlink("Engine/Version.h");
+	system("cp Makefile Makefile.bak") if (-r "Makefile");
+	my $compiler = "g++ ";
+	$compiler = " mpicxx " if ($mpi);
+	my $fh;
+	open($fh,">Makefile") or die "Cannot open Makefile for writing: $!\n";
+	my $usePthreadsOrNot = " ";
+	$usePthreadsOrNot = " -DUSE_PTHREADS " if ($pthreads);
+	my $optimizations = " -O3 -DNDEBUG ";
+	$optimizations = " -g3 -D_GLIBCXX_DEBUG " if ($build eq "debug");
+	$optimizations .= " -g3 " if ($build eq "callgrind");
+	my $strip = "strip ";
+	$strip = " true " if ($build eq "debug" or $build eq "callgrind");
+
+
+	my $cppflags= "-Werror -Wall -Wstrict-overflow=5  -IEngine  ";
+	$cppflags .= "  -I$PsimagLite/src -I$PsimagLite $usePthreadsOrNot $floating";
+
+	Make::make($fh,\@drivers,"DMRG++",$platform,$mpi,"$lapack $pthreadsLib",
+	"$compiler $optimizations",$cppflags,$strip,"Engine/Version.h",
+	"Engine/Version.h gitrev","");
+	local *FH = $fh;
+print FH<<EOF;
+
+Engine/Version.h: gitrev
+	./gitrev > Engine/Version.h
+
+EOF
+
+	close($fh);
+	print STDERR "$0: File Makefile has been written\n";
 }
 
-sub getConcurrencyName()
-{
-	my $concurrencyName = "UNKNOWN";
-
-	if ($mpi) {
-		$concurrencyName="ConcurrencyMpi";
-	} else {
-		$concurrencyName="ConcurrencySerial";
-	}
-	return $concurrencyName;
-}
-
-sub compilerName
-{
-	return "g++";
-	my @tryThis = ("g++","g++4");
-	my $ret;
-	my $compiler;
-	foreach my $comp (@tryThis) {
-		my $ret = system("$comp &>2 /dev/null");
-		if ($ret==0) {
-			$compiler = $comp;
-			last;
-		} 
-			
-	}
-	return $compiler if defined $compiler; 
-	die "$0: No suitable compiler found\n";
-}
 

cbc6cdb99fc988cdc1acc33c3f77fcf4be21c3ca
g1257 <gz1@ornl.gov>
2015-05-06 13:56:51 -0400

    Config.make based compilation now

--- src/configure.pl
+++ src/configure.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl
 =pod
-Copyright (c) 2009-2014, UT-Battelle, LLC
+Copyright (c) 2009-2015, UT-Battelle, LLC
 All rights reserved
 
-[Lanczos++, Version 1.0.0]
+[Lanczos++, Version 1.0]
 
----------------------------------------------------------------
+*********************************************************
 THE SOFTWARE IS SUPPLIED BY THE COPYRIGHT HOLDERS AND
 CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
 WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -13,7 +13,7 @@ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
 PARTICULAR PURPOSE ARE DISCLAIMED.
 
 Please see full open source license included in file LICENSE.
----------------------------------------------------------------
+*********************************************************
 
 =cut
 use warnings;
@@ -22,59 +22,33 @@ use strict;
 use lib "../../PsimagLite/scripts";
 use Make;
 
-my $mpi=0;
-my $platform=guessPlatform();
-my @drivers = ("lanczos","thermal","lorentzian");
-my $lapack=Make::findLapack();
-my $PsimagLite="../../PsimagLite";
-my ($pthreads,$pthreadsLib)=(0,"");
-my $brand= "v1.0";
-my $build="production";
-my $floating="";
+my ($arg) = @ARGV;
 
-system("make clean");
+if (defined($arg) and -r "$arg" and $arg ne "Config.make") {
+	my $cmd = "cp Config.make Config.make.bak";
+	system($cmd);
+	$cmd = "cp $arg Config.make";
+	system($cmd);
+}
+
+my @drivers = ("lanczos","thermal","lorentzian");
 
 createMakefile();
-
-sub guessPlatform
-{
-	my $platform="Linux";
-	$platform="Darwin" if (isAMac());
-	return $platform;
-}
-
-sub isAMac
-{
-	open(PIPE,"uname -a |grep -i Darwin") or return 0;
-	$_=<PIPE>;
-	close(PIPE);
-
-	return 1 unless ($_ eq "" or $_ eq "\n");
-	return 0;
-}
 
 sub createMakefile
 {
 	unlink("Engine/Version.h");
 	Make::backupMakefile();
-	my $compiler = "g++ ";
-	$compiler = " mpicxx " if ($mpi);
+	if (!(-r "Config.make")) {
+		my $cmd = "cp ../TestSuite/inputs/Config.make Config.make";
+		system($cmd);
+		print STDERR "$0: Executed $cmd\n";
+	}
+
 	my $fh;
 	open($fh,">Makefile") or die "Cannot open Makefile for writing: $!\n";
-	my $usePthreadsOrNot = " ";
-	$usePthreadsOrNot = " -DUSE_PTHREADS " if ($pthreads);
-	my $optimizations = " -O3 -DNDEBUG ";
-	$optimizations = " -g3 -D_GLIBCXX_DEBUG " if ($build eq "debug");
-	$optimizations .= " -g3 " if ($build eq "callgrind");
-	my $strip = "strip ";
-	$strip = " true " if ($build eq "debug" or $build eq "callgrind");
 
-
-	my $cppflags= " -IEngine  ";
-	$cppflags .= "  -I$PsimagLite/src -I$PsimagLite $usePthreadsOrNot $floating";
-
-	Make::make($fh,\@drivers,"Lanczos++",$platform,$mpi,"$lapack $pthreadsLib -lpsimaglite",
-	"$compiler $optimizations",$cppflags,$strip,"Engine/Version.h",
+	Make::newMake($fh,\@drivers,"Lanczos++","Engine/Version.h",
 	"Engine/Version.h gitrev","");
 	local *FH = $fh;
 print FH<<EOF;
@@ -88,7 +62,6 @@ Engine/Version.h: gitrev
 EOF
 
 	close($fh);
-	print STDERR "$0: File Makefile has been written\n";
+	print STDERR "File Makefile has been written\n";
 }
-
 

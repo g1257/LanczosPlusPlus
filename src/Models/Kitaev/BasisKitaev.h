0576479369ef714f9d8ee001dfa02fc247f456ca
Gonzalo Alvarez <gz1@ornl.gov>
2018-03-27 16:29:15 -0400

    Kitaev model started

---
+++ src/Models/Kitaev/BasisKitaev.h
@@ -0,0 +1,314 @@
+/*
+*/
+
+#ifndef LANCZOS_BASIS_HEISENBERG_H
+#define LANCZOS_BASIS_HEISENBERG_H
+
+#include "BitManip.h"
+#include "ProgramGlobals.h"
+#include "../../Engine/BasisBase.h"
+
+namespace LanczosPlusPlus {
+
+template<typename GeometryType>
+class BasisHeisenberg : public BasisBase<GeometryType> {
+
+	typedef ProgramGlobals::PairIntType PairIntType;
+
+public:
+
+	typedef BasisBase<GeometryType> BaseType;
+	typedef typename BaseType::WordType WordType;
+	typedef typename BaseType::VectorWordType VectorWordType;
+	static VectorWordType bitmask_;
+
+	enum {OPERATOR_NIL=ProgramGlobals::OPERATOR_NIL,
+		  OPERATOR_SZ=ProgramGlobals::OPERATOR_SZ};
+
+	BasisHeisenberg(const GeometryType& geometry,
+	                SizeType twiceS,
+	                SizeType szPlusConst)
+	    : geometry_(geometry),
+	      twiceS_(twiceS),
+	      szPlusConst_(szPlusConst),
+	      bits_(0)
+	{
+		SizeType sites = geometry_.numberOfSites();
+		assert(bitmask_.size()==0 || bitmask_.size()== sites);
+		if (bitmask_.size()==0) doBitmask();
+		WordType searchTotal = 1;
+		assert(twiceS > 0);
+		bits_ = 1 + static_cast<SizeType>(logBase2(twiceS + 1));
+		if (twiceS & 1) bits_--;
+		searchTotal <<= (bits_ * sites);
+
+		WordType mask = getMask();
+
+		for (WordType lui = 0; lui < searchTotal; ++lui) {
+			int tmp = mOf(lui,mask);
+			if (tmp < 0 || static_cast<SizeType>(tmp) != szPlusConst) continue;
+			data_.push_back(lui);
+		}
+	}
+
+	PairIntType parts() const
+	{
+		return PairIntType(twiceS_, szPlusConst_);
+	}
+
+	static const WordType& bitmask(SizeType i)
+	{
+		return bitmask_[i];
+	}
+
+	SizeType size() const { return data_.size(); }
+
+	SizeType dofs() const { return twiceS_ + 1; }
+
+	virtual SizeType hilbertOneSite(SizeType) const
+	{
+		return 1 + twiceS_;
+	}
+
+	SizeType perfectIndex(const VectorWordType&) const
+	{
+		throw PsimagLite::RuntimeError("BasisHeisenberg::perfectIndex kets\n");
+	}
+
+	SizeType perfectIndex(WordType ket,WordType) const
+	{
+		for (SizeType i = 0; i < data_.size(); ++i) {
+			if (ket == data_[i]) return i;
+		}
+
+		throw PsimagLite::RuntimeError("perfectIndex: no index found\n");
+	}
+
+	WordType operator()(SizeType i, SizeType) const
+	{
+		return data_[i];
+	}
+
+	SizeType isThereAnElectronAt(WordType,
+	                             WordType,
+	                             SizeType,
+	                             SizeType,
+	                             SizeType) const
+	{
+		throw PsimagLite::RuntimeError("BasisHeisenberg::isThereAnElectronAt\n");
+	}
+
+	SizeType getN(WordType ket1,
+	              WordType,
+	              SizeType site,
+	              SizeType,
+	              SizeType) const
+	{
+		WordType mask = getMask();
+		ket1 >>= (bits_ * site);
+		return (ket1 & mask);
+	}
+
+	int doSignGf(WordType, WordType,SizeType,SizeType,SizeType) const
+	{
+		throw PsimagLite::RuntimeError("BasisHeisenberg::doSignGf\n");
+	}
+
+	int doSign(WordType,
+	           WordType,
+	           SizeType,
+	           SizeType,
+	           SizeType,
+	           SizeType,
+	           SizeType) const
+	{
+		throw PsimagLite::RuntimeError("BasisHeisenberg::doSign\n");
+	}
+
+	PairIntType getBraIndex(WordType ket1,
+	                        WordType ket2,
+	                        SizeType operatorLabel,
+	                        SizeType site,
+	                        SizeType spin,
+	                        SizeType orb) const
+	{
+		if (twiceS_ != 1)
+			throw PsimagLite::RuntimeError("BasisHeisenberg::getBraIndex_ \n");
+
+		if (operatorLabel == ProgramGlobals::OPERATOR_SPLUS ||
+		        operatorLabel == ProgramGlobals::OPERATOR_SMINUS) {
+			return getBraIndexSplusSminus(ket1,ket2,operatorLabel,site,spin,orb);
+		}
+
+		return getBraIndex_(ket1,ket2,operatorLabel,site,spin,orb);
+	}
+
+	SizeType orbsPerSite(SizeType) const { return 1; }
+
+	SizeType orbs() const { return 1; }
+
+	SizeType perfectIndex(WordType,SizeType,SizeType) const
+	{
+		throw PsimagLite::RuntimeError("BasisHeisenberg::perfectIndex\n");
+	}
+
+	void print(std::ostream& os, typename BaseType::PrintEnum binaryOrDecimal) const
+	{
+		SizeType hilbert = 1;
+		hilbert <<= geometry_.numberOfSites();
+		if (binaryOrDecimal == BaseType::PRINT_BINARY) {
+			ProgramGlobals::printBasisBinary(os,hilbert,data_);
+		} else {
+			ProgramGlobals::printBasisDecimal(os,40,data_);
+		}
+	}
+
+	SizeType szPlusConst() const { return szPlusConst_; }
+
+	template<typename GeometryType2>
+	friend std::ostream& operator<<(std::ostream& os,
+	                                const BasisHeisenberg<GeometryType2>& basis);
+
+private:
+
+	bool getBra(WordType& bra,
+	            WordType ket,
+	            WordType site1,
+	            SizeType val1,
+	            SizeType site2,
+	            SizeType val2) const
+	{
+		bra = ket;
+		WordType mask1 = getMask();
+		WordType mask2 = mask1;
+		mask1 <<= (site1*bits_);
+		bra &= (~mask1);
+
+		mask2 <<= (site2*bits_);
+		bra &= (~mask2);
+
+		mask1 = val1;
+		mask1 <<= (site1*bits_);
+		bra |= mask1;
+
+		mask2 = val2;
+		mask2 <<= (site2*bits_);
+		bra |= mask2;
+
+		return true;
+	}
+
+
+	WordType getMask() const
+	{
+		SizeType mask = 1;
+		for (SizeType i = 0; i < bits_; ++i)
+			mask |= bitmask_[i];
+		return mask;
+	}
+
+	int mOf(WordType lui, WordType mask) const
+	{
+		SizeType m = 0;
+		bool allowed = true;
+		while (lui != 0) {
+			WordType tmp = (lui & mask);
+			if (!mOfIsAllowed(tmp)) {
+				allowed = false;
+				break;
+			}
+
+			m += tmp;
+			lui >>= bits_;
+		}
+
+		return (allowed) ? m : -1;
+	}
+
+	bool mOfIsAllowed(WordType val) const
+	{
+		if (twiceS_ & 1) return true;
+
+		return (val <= twiceS_);
+	}
+
+	PairIntType getBraIndexSplusSminus(WordType ket1,
+	                                   WordType ket2,
+	                                   SizeType operatorLabel,
+	                                   SizeType site,
+	                                   SizeType,
+	                                   SizeType) const
+	{
+		assert(operatorLabel == ProgramGlobals::OPERATOR_SPLUS ||
+		       operatorLabel == ProgramGlobals::OPERATOR_SMINUS);
+
+		WordType mask1 = getMask();
+		mask1 <<= (site*bits_);
+
+		if (ket1 & mask1) {
+			if (operatorLabel == ProgramGlobals::OPERATOR_SPLUS)
+				return PairIntType(-1,0);
+		} else {
+			if (operatorLabel == ProgramGlobals::OPERATOR_SMINUS)
+				return PairIntType(-1,0);
+		}
+
+		WordType bra = ket1 ^ mask1;
+
+		return PairIntType(perfectIndex(bra,ket2),1);
+	}
+
+	PairIntType getBraIndex_(WordType ket1,
+	                         WordType ket2,
+	                         SizeType operatorLabel,
+	                         SizeType site,
+	                         SizeType spin,
+	                         SizeType orb) const
+	{
+		if (operatorLabel != ProgramGlobals::OPERATOR_N || twiceS_ != 1)
+			throw PsimagLite::RuntimeError("BasisHeisenberg::getBraIndex_ \n");
+
+		SizeType nup = getN(ket1,ket2,site,0,orb);
+		assert(nup < 2);
+		return PairIntType(perfectIndex(ket1,ket2),
+		                   (spin == ProgramGlobals::SPIN_UP) ? nup : 1 - nup);
+	}
+
+	void doBitmask()
+	{
+		SizeType n = geometry_.numberOfSites();
+		bitmask_.resize(n);
+		bitmask_[0]=1ul;
+		for (SizeType i=1;i<n;i++)
+			bitmask_[i] = bitmask_[i-1]<<1;
+	}
+
+	SizeType logBase2(SizeType x) const
+	{
+		int ret = 0;
+		while (x >>= 1) ++ret;
+		return ret;
+	}
+
+	const GeometryType& geometry_;
+	SizeType twiceS_;
+	SizeType szPlusConst_;
+	SizeType bits_;
+	VectorWordType data_;
+}; // class BasisHeisenberg
+
+template<typename GeometryType>
+std::ostream& operator<<(std::ostream& os,const BasisHeisenberg<GeometryType>& basis)
+{
+	for (SizeType i=0;i<basis.data_.size();i++)
+		os<<i<<" "<<basis.data_[i]<<"\n";
+	return os;
+}
+
+template<typename GeometryType>
+typename BasisHeisenberg<GeometryType>::VectorWordType
+BasisHeisenberg<GeometryType>::bitmask_;
+
+} // namespace LanczosPlusPlus
+#endif
+
---
+++ src/Models/Kitaev/Kitaev.h
@@ -0,0 +1,347 @@
+/*
+*/
+
+#ifndef LANCZOS_HEISENBERG_H
+#define LANCZOS_HEISENBERG_H
+
+#include "CrsMatrix.h"
+#include "BitManip.h"
+#include "TypeToString.h"
+#include "SparseRow.h"
+#include "BasisHeisenberg.h"
+#include "ParametersHeisenberg.h"
+#include "ModelBase.h"
+
+namespace LanczosPlusPlus {
+
+template<typename ComplexOrRealType,typename GeometryType,typename InputType>
+class Heisenberg  : public ModelBase<ComplexOrRealType,GeometryType,InputType> {
+
+	typedef typename PsimagLite::Real<ComplexOrRealType>::Type RealType;
+	typedef PsimagLite::Matrix<ComplexOrRealType> MatrixType;
+	typedef std::pair<SizeType,SizeType> PairType;
+	typedef ModelBase<ComplexOrRealType,GeometryType,InputType> BaseType;
+
+	enum {SPIN_UP = ProgramGlobals::SPIN_UP, SPIN_DOWN = ProgramGlobals::SPIN_DOWN};
+
+public:
+
+	typedef ParametersHeisenberg<RealType,InputType> ParametersModelType;
+	typedef BasisHeisenberg<GeometryType> BasisType;
+	typedef typename BasisType::BaseType BasisBaseType;
+	typedef typename BasisType::WordType WordType;
+	typedef typename BaseType::SparseMatrixType SparseMatrixType;
+	typedef typename BaseType::VectorType VectorType;
+	typedef PsimagLite::SparseRow<SparseMatrixType> SparseRowType;
+
+	Heisenberg(SizeType szPlusConst,
+	           InputType& io,
+	           const GeometryType& geometry)
+	    : mp_(io),
+	      geometry_(geometry),
+	      basis_(geometry,mp_.twiceTheSpin,szPlusConst),
+	      jpm_(geometry_.numberOfSites(),geometry_.numberOfSites()),
+	      jzz_(geometry_.numberOfSites(),geometry_.numberOfSites())
+	{
+		SizeType n = geometry_.numberOfSites();
+
+		if (geometry_.terms() != 2) {
+			PsimagLite::String msg("Heisenberg: must have either 2 terms\n");
+			throw PsimagLite::RuntimeError(msg);
+		}
+
+		for (SizeType i=0;i<n;i++) {
+			for (SizeType j=0;j<n;j++) {
+				jpm_(i,j) = geometry_(i,0,j,0,0);
+				jzz_(i,j) = geometry_(i,0,j,0,1);
+			}
+		}
+	}
+
+	~Heisenberg()
+	{
+		BaseType::deleteGarbage(garbage_);
+	}
+
+	SizeType size() const { return basis_.size(); }
+
+	SizeType orbitals(SizeType) const
+	{
+		return 1;
+	}
+
+	void setupHamiltonian(SparseMatrixType& matrix) const
+	{
+		setupHamiltonian(matrix,basis_);
+	}
+
+	//! Gf. related functions below:
+	void setupHamiltonian(SparseMatrixType& matrix,
+	                      const BasisBaseType& basis) const
+	{
+		SizeType hilbert=basis.size();
+		typename PsimagLite::Vector<RealType>::Type diag(hilbert,0.0);
+		calcDiagonalElements(diag,basis);
+
+		SizeType nsite = geometry_.numberOfSites();
+		SizeType dummy = 0;
+		SizeType orb = 0;
+
+		matrix.resize(hilbert,hilbert);
+		// Calculate off-diagonal elements AND store matrix
+		SizeType nCounter=0;
+		for (SizeType ispace=0;ispace<hilbert;ispace++) {
+			SparseRowType sparseRow;
+			matrix.setRow(ispace,nCounter);
+
+			WordType ket = basis(ispace,dummy);
+			// Save diagonal
+			sparseRow.add(ispace,diag[ispace]);
+			for (SizeType i=0;i<nsite;i++) {
+				SizeType val1 = basis.getN(ket,dummy,i,dummy,orb);
+				if (val1 == mp_.twiceTheSpin) continue;
+				val1++;
+				setSplusSminus(sparseRow,ket,i,val1,basis);
+			}
+
+			nCounter += sparseRow.finalize(matrix);
+		}
+
+		matrix.setRow(hilbert,nCounter);
+		matrix.checkValidity();
+		assert(isHermitian(matrix));
+	}
+
+	bool hasNewParts(std::pair<SizeType,SizeType>& newParts,
+	                 const std::pair<SizeType,SizeType>& oldParts,
+	                 SizeType what,
+	                 SizeType spin,
+	                 SizeType orb) const
+	{
+		if (what==ProgramGlobals::OPERATOR_SZ)
+			return false;
+
+		if (what == ProgramGlobals::OPERATOR_SPLUS ||
+		        what == ProgramGlobals::OPERATOR_SMINUS)
+			return hasNewPartsSplusOrMinus(newParts,oldParts,what,spin);
+
+		PsimagLite::String str(__FILE__);
+		str += " " + ttos(__LINE__) +  "\n";
+		str += PsimagLite::String("hasNewParts: unsupported operator ");
+		str += ProgramGlobals::id2Operator(what) + "\n";
+		throw std::runtime_error(str.c_str());
+	}
+
+	const GeometryType& geometry() const { return geometry_; }
+
+	const BasisType& basis() const { return basis_; }
+
+	void printBasis(std::ostream& os) const
+	{
+		os<<basis_;
+	}
+
+	PsimagLite::String name() const { return __FILE__; }
+
+	BasisType* createBasis(SizeType nup, SizeType ndown) const
+	{
+		BasisType* ptr = new BasisType(geometry_,nup,ndown);
+		garbage_.push_back(ptr);
+		return ptr;
+	}
+
+	void print(std::ostream& os) const { os<<mp_; }
+
+	void printOperators(std::ostream& os) const
+	{
+		SizeType sites = geometry_.numberOfSites();
+		SizeType nup = basis_.szPlusConst();
+		SizeType ndown = sites - nup;
+		os<<"#SectorSource 2 "<<nup<<" "<<ndown<<"\n";
+		for (SizeType site = 0; site < sites; ++site)
+			printOperatorSz(site,os);
+	}
+
+private:
+
+	void printOperatorSz(SizeType site, std::ostream& os) const
+	{
+		SizeType sites = geometry_.numberOfSites();
+		SizeType nup = basis_.szPlusConst();
+		SizeType ndown = sites - nup;
+		MatrixType matrix;
+		setupOperator(matrix,"sz",site);
+		os<<"#Operator_Sz_"<<"_"<<site<<"\n";
+		os<<"#SectorDest 2 "<<nup<<" "<<ndown<<"\n";
+		os<<"#Matrix\n";
+		os<<matrix;
+	}
+
+	void setupOperator(MatrixType& matrix,
+	                   PsimagLite::String operatorName,
+	                   SizeType site) const
+	{
+		SizeType hilbert = basis_.size();
+		SizeType nsite = geometry_.numberOfSites();
+
+		if (operatorName != "sz") {
+			PsimagLite::String str(__FILE__);
+			str += " " + ttos(__LINE__) + "\n";
+			str += "operator " + operatorName + " is unimplemented for this model\n";
+			throw PsimagLite::RuntimeError(str);
+		}
+
+		if (site >= nsite) {
+			PsimagLite::String str(__FILE__);
+			str += " " + ttos(__LINE__) + "\n";
+			str += "site requested " + ttos(site);
+			str += " but number of sites= " + ttos(nsite) + "\n";
+			throw PsimagLite::RuntimeError(str);
+		}
+
+		matrix.resize(hilbert,hilbert);
+		matrix.setTo(0.0);
+
+		SizeType orb = 0;
+		SizeType dummy = 0;
+		for (SizeType ispace=0;ispace<hilbert;ispace++) {
+			WordType ket = basis_(ispace,dummy);
+			// assumes OPERATOR_SZ
+			SizeType val1 = basis_.getN(ket,dummy,site,dummy,orb);
+			RealType tmp = val1 - mp_.twiceTheSpin*0.5;
+
+			matrix(ispace,ispace) = tmp;
+		}
+	}
+
+	bool hasNewPartsSplusOrMinus(std::pair<SizeType,SizeType>& newParts,
+	                             const std::pair<SizeType,SizeType>& oldParts,
+	                             SizeType what,
+	                             SizeType) const
+	{
+		if (mp_.twiceTheSpin != 1)
+			throw PsimagLite::RuntimeError("BasisHeisenberg::hasNewPartsSplusOrMinus\n");
+
+		newParts.first = oldParts.first;
+		bool flag = false;
+		if (what == ProgramGlobals::OPERATOR_SPLUS) {
+			newParts.second = oldParts.second + 1;
+			if (newParts.second > geometry_.numberOfSites())
+				flag = true;
+		} else if (what == ProgramGlobals::OPERATOR_SMINUS) {
+			if (basis_.szPlusConst() == 0) flag =true;
+			newParts.second = oldParts.second - 1;
+		}
+
+		if (!flag) return true;
+
+		throw PsimagLite::RuntimeError("BasisHeisenberg:: S+ to all ups or S- to all downs\n");
+	}
+
+	void calcDiagonalElements(typename PsimagLite::Vector<RealType>::Type& diag,
+	                          const BasisBaseType& basis) const
+	{
+		SizeType hilbert=basis.size();
+		SizeType nsite = geometry_.numberOfSites();
+		SizeType orb = 0;
+		SizeType dummy = 0;
+
+		// Calculate diagonal elements
+		for (SizeType ispace=0;ispace<hilbert;ispace++) {
+			WordType ket = basis(ispace,dummy);
+			ComplexOrRealType s=0;
+			for (SizeType i=0;i<nsite;i++) {
+
+				SizeType val1 = basis.getN(ket,dummy,i,dummy,orb);
+				RealType tmp1 = val1 - mp_.twiceTheSpin*0.5;
+				RealType tmp1d = tmp1*tmp1;
+
+				if (i < mp_.magneticField.size()) s += mp_.magneticField[i]*tmp1;
+				if (i < mp_.anisotropy.size()) s += mp_.anisotropy[i]*tmp1d;
+
+				for (SizeType j=i+1;j<nsite;j++) {
+
+					SizeType val2 = basis.getN(ket,dummy,j,dummy,orb);
+					RealType tmp2 = val2 - mp_.twiceTheSpin*0.5;
+
+					// Sz Sz term:
+					s += tmp1*tmp2*jzz_(i,j);
+				}
+			}
+
+			assert(fabs(PsimagLite::imag(s))<1e-12);
+			diag[ispace] = PsimagLite::real(s);
+		}
+	}
+
+	void setSplusSminus(SparseRowType &sparseRow,
+	                    const WordType& ket,
+	                    SizeType i,
+	                    SizeType val1,
+	                    const BasisBaseType &basis) const
+	{
+		SizeType nsite = geometry_.numberOfSites();
+		SizeType dummy = 0;
+		SizeType orb = 0;
+		RealType spin = mp_.twiceTheSpin*0.5;
+
+		for (SizeType j=0;j<nsite;j++) {
+			if (i == j) continue;
+			if (jpm_(i,j) == 0.0) continue;
+
+			SizeType val2 = basis.getN(ket,dummy,j,dummy,orb);
+			if (val2 == 0) continue;
+			RealType m2 = val2 - spin;
+			val2--;
+			RealType m1 = val2 - spin;
+			WordType bra;
+
+			basis.getBra(bra,ket,i,val1,j,val2);
+			SizeType temp = basis.perfectIndex(bra,dummy);
+			RealType tmp = sqrt(spin*(spin+1.0) - m1*(m1+1.0));
+			tmp *= sqrt(spin*(spin+1.0) - m2*(m2-1.0));
+			sparseRow.add(temp,0.5*tmp*jpm_(i,j));
+		}
+	}
+
+	RealType signSplusSminus(SizeType i,
+	                         SizeType j,
+	                         const WordType& bra1,
+	                         const WordType& bra2) const
+	{
+		SizeType n = geometry_.numberOfSites();
+		int s = 1;
+		if (j>0) s *= parityFrom(0,j-1,bra2);
+		if (i>0) s *= parityFrom(0,i-1,bra2);
+		if (i<n-1) s *= parityFrom(i+1,n-1,bra1);
+		if (j<n-1) s *= parityFrom(j+1,n-1,bra1);
+		return s;
+	}
+
+	// from i to j including i and j
+	// assumes i<=j
+	int parityFrom(SizeType i,SizeType j,const WordType& ket) const
+	{
+		if (i==j) return (BasisType::bitmask(j) & ket) ? -1 : 1;
+		assert(i<j);
+		//j>i>=0 now
+		WordType mask = ket;
+		mask &= ((1 << (i+1)) - 1) ^ ((1 << j) - 1);
+		int s=(PsimagLite::BitManip::count(mask) & 1) ? -1 : 1;
+		// Is there something of this species at i?
+		if (BasisType::bitmask(i) & ket) s = -s;
+		// Is there something of this species at j?
+		if (BasisType::bitmask(j) & ket) s = -s;
+		return s;
+	}
+
+	const ParametersModelType mp_;
+	const GeometryType& geometry_;
+	BasisType basis_;
+	PsimagLite::Matrix<ComplexOrRealType> jpm_;
+	PsimagLite::Matrix<ComplexOrRealType> jzz_;
+	mutable typename PsimagLite::Vector<BasisType*>::Type garbage_;
+}; // class Heisenberg
+} // namespace LanczosPlusPlus
+#endif
+
